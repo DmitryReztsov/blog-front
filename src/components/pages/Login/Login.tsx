096c303d4d55beeadcf6cdc72299bf7f289cfd95
DmitryReztsov <rez_dima@mail.ru>
2022-01-20 21:44:21 +0300

    добавлена страница логина

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -1,16 +1,23 @@
 import React, {FC} from 'react';
 import Container from "../../Container/Container";
 import {Link} from "react-router-dom";
+import './Login.css'
 
 const Login: FC = () => {
     return (
-        <div className={'login'}>
+        <main className={'login'}>
             <Container>
-                <h2 className={'login__header'}>Sign in</h2>
-                <Link to={"/register"}>Need an account?</Link>
-
+                <div className={'login__body'}>
+                    <h2 className={'login__header'}>Sign in</h2>
+                    <Link className={'login__register-link'} to={"/register"}>Need an account?</Link>
+                    <form className={'login__form'} action="">
+                        <input className={'login__input'} type="email"/>
+                        <input className={'login__input'} type="password"/>
+                        <button className={'login__submit'} type={"submit"}>Sign in</button>
+                    </form>
+                </div>
             </Container>
-        </div>
+        </main>
     );
 };
 
