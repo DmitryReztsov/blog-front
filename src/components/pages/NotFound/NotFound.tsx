6034d8c2d9ca81d4b17c4c98bb18c748be356681
DmitryReztsov <rez_dima@mail.ru>
2022-01-22 14:11:33 +0300

    добавлены стили 404 странице, добавлена вкладка assets в public для картинок

--- src/components/pages/NotFound/NotFound.tsx
+++ src/components/pages/NotFound/NotFound.tsx
@@ -1,13 +1,16 @@
 import React, {FC} from 'react';
 import Container from "../../Container/Container";
+import './NotFound.css'
 
 const NotFound: FC = () => {
     return (
-        <div>
+        <div className={'not-found'}>
             <Container>
-                <h2>404</h2>
-                <p>Page not found</p>
-                <p>The site configured at this address does not contain the requested file.</p>
+                <div className={'not-found__body'}>
+                    <img src="/assets/404.svg" alt="404 error" className={'not-found__img'}/>
+                    <h2 className={'not-found__subtitle'}>Page not found</h2>
+                    <p className={'not-found__text'}>The site configured at this address does not contain the requested file.</p>
+                </div>
             </Container>
         </div>
     );
