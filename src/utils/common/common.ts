74f6ca5b73bc428860e9261a670069c6c511c049
FominNV <90246435+FominNV@users.noreply.github.com>
2022-01-31 16:47:49 +0300

    Update common.ts

--- src/utils/common/common.ts
+++ src/utils/common/common.ts
@@ -7,6 +7,50 @@ export const getToken = (): string | null => {
 
 // парсим тело ответа, если что-то пошло не так
 // пример тела: {"errors":{"email":["has already been taken"],"username":["has already been taken"]}}
+
+export function parseError(error: any): string[] {
+  const objError = error['errors'];
+  const result: string[] = [];
+
+  // почему-то сервер возвращает ошибки в странном формате, иногда это массив, иногда просто строка,
+  // поэтому делаем ветвление
+  for (const key in objError) {
+    if (typeof objError[key] === 'object') {
+      result.push(key + ' ' + objError[key][0]);
+    } else {
+      result.push(key + ' ' + objError[key]);
+    }
+  }
+  return result;
+}
+
+export function setCookie(name: string, value: string, options?: { [key: string]: string }) {
+  options = {
+    path: '/',
+    ...options,
+  };
+
+  let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+
+  for (const optionKey in options) {
+    if (options) {
+      updatedCookie += '; ' + optionKey;
+      const optionValue = options[optionKey];
+      if (optionValue) {
+        updatedCookie += '=' + optionValue;
+      }
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+export function deleteCookie() {
+  setCookie('jwtToken', '', {
+    'max-age': '-1',
+  });
+}
+
 export const dateFormat = (unformatDate: string): string => {
   const months = [
     'January',
@@ -25,20 +69,3 @@ export const dateFormat = (unformatDate: string): string => {
   const date = new Date(unformatDate);
   return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 };
-
-export function parseError(error: any): string[] {
-  const objError = error['errors'];
-  const result: string[] = [];
-
-  // почему-то сервер возвращает ошибки в странном формате, иногда это массив, иногда просто строка,
-  // поэтому делаем ветвление
-  for (const key in objError) {
-    if (typeof objError[key] === 'object') {
-      result.push(key + ' ' + objError[key][0]);
-    } else {
-      result.push(key + ' ' + objError[key]);
-    }
-  }
-  return result;
-}
-
